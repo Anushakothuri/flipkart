080e8aef04eb6c9e19b6456a9981c1858c3d8319
Anushakothuri <133662888+Anushakothuri@users.noreply.github.com>
2023-10-12 14:47:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           <code>Hi i am Anusha</code> 
+           <code>Welcome!..</code> 
         </p>
         <a
           className="App-link"
